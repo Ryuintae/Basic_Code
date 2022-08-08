bc87085bba05ffb42488e501f2e7867e3f19adb4
Ryuintae <101119025+Ryuintae@users.noreply.github.com>
2022-08-09 06:04:59 +0900

    Update part4.java

--- src/For/part4.java
+++ src/For/part4.java
@@ -2,7 +2,7 @@ package For;
 
 public class part4 {
 	public static void main(String[] args) {
-//출력예) 	 2 * 1 =  2          3 * 1 =  3          4 * 1 =  4
+//출력예)  2 * 1 =  2          3 * 1 =  3          4 * 1 =  4
 //	    2 * 2 =  4          3 * 2 =  6          4 * 2 =  8
 //          2 * 9 = 18          3 * 9 = 27          4 * 9 = 36
 		for (int i = 1; i <= 9; i++) {
